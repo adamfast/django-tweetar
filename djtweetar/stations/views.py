e98161f9c3345f0cd6f9149881950d608186c7dd
Adam Fast <adam@adamfast.com>
2010-10-01 02:45:55 +0000

    Add signal to automatically tweet a METAR when the object is saved (which should only be on creation.)

--- djtweetar/stations/views.py
+++ djtweetar/stations/views.py
@@ -1,4 +1,6 @@
+from django.db.models.signals import post_save
 from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
@@ -6,6 +8,7 @@ from django.template import RequestContext
 from djtweetar.stations.forms import *
 from socialregistration.models import TwitterProfile
 from weathertracking.models import WeatherStation
+from tweetar import *
 
 def main(request):
     form = StationRegistrationForm()
@@ -44,3 +47,22 @@ def connect(request, station):
     station = get_object_or_404(Station, code=station)
 
     return render_to_response('connect.html', {'socialregistration_connect_object': station,}, context_instance=RequestContext(request))
+
+def auto_tweet(sender, **kwargs):
+    """Run when a socialprofile is saved - on this site, that should only be on initial creation."""
+    profile = kwargs['instance']
+
+    conf = {
+        'station': profile.content_object.code,
+        'twitter_user': profile.screenname,
+        'oauth_consumer_key': settings.TWITTER_CONSUMER_KEY,
+        'oauth_consumer_secret': settings.TWITTER_CONSUMER_SECRET_KEY,
+        'access_token_key': profile.consumer_key,
+        'access_token_secret': profile.consumer_secret,
+    }
+    try:
+        retrieve_and_post(conf)
+    except urllib2.HTTPError:
+        pass
+
+post_save.connect(auto_tweet, sender=TwitterProfile)

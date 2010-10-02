1c167c215d0ab5092f1a881a933d48e9743904d3
Adam Fast <adam@adamfast.com>
2010-10-02 12:43:51 -0500

    Register the admin class, not the model.

--- djtweetar/runlogs/admin.py
+++ djtweetar/runlogs/admin.py
@@ -4,4 +4,4 @@ from djtweetar.runlogs.models import *
 class TweetarRunAdmin(admin.ModelAdmin):
     list_display = ('begin', 'end', 'elapsed_seconds', 'total_stations', 'stations_updated', 'station_exceptions')
 
-admin.site.register(TweetarRun, TweetarRun)
+admin.site.register(TweetarRun, TweetarRunAdmin)

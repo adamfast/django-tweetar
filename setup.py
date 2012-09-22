c1ceec538148eb7ea8a585229b16a73cfe4584ab
Adam Fast <adam@adamfast.com>
2012-09-21 23:48:14 -0500

    Include a basic setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = "django-tweetar",
+    url = "http://github.com/adamfast/django-tweetar",
+    author = "Adam Fast",
+    author_email = "adamfast@gmail.com",
+    version = "0.1",
+    license = "BSD",
+    packages = ["djtweetar"],
+    install_requires = ['python-tweetar'],
+    description = "App for posting current conditions to twitter via python-tweetar.",
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)
